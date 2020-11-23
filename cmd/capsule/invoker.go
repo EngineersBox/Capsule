a378cae5c482cd03aade63b7ab80a500954be990
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2020-11-24 00:20:04 +1100

    Added cgroup creation for spawned containers

--- cmd/capsule/invoker.go
+++ cmd/capsule/invoker.go
@@ -1,18 +1,19 @@
 package main
 
-// InvParams ... Generic parameters for an invocable function
-type InvParams interface{}
-
 // Invocable ... Abstract generic function that can be invoked
-type Invocable func(...InvParams) error
+type Invocable func(...interface{}) error
 
 // Call ... Invoke a function with a panic condition on erroring
 //
 // Params:
 // - Invocable inv: Function to invoke
 // - ...InvParams params: Parameters to pass to given function
-func call(inv Invocable, params ...InvParams) {
-	if err := inv(params); err != nil {
+func call(inv Invocable, params ...interface{}) {
+	handleErrors(inv(params))
+}
+
+func handleErrors(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
--- cmd/capsule/main.go
+++ cmd/capsule/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
+)
+
+var (
+	cid      = "container"
+	fsname   = "rootfs"
+	proc_max = "20"
 )
 
 func main() {
@@ -18,9 +27,12 @@ func main() {
 	}
 }
 
-// Run ... Fork-execute a fs instance
+// fun ... Fork-execute a fs instance
 func run() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	cgroup()
+
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
@@ -34,20 +46,29 @@ func run() {
 	}
 }
 
-// Child ... Spawn a child process within the fs instance created via run()
+// child ... Spawn a child process within the fs instance created via run()
 func child() {
-	call(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
-	call(os.MkdirAll("rootfs/oldrootfs", 0700))
-	call(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
-	call(os.Chdir("/"))
+	handleErrors(syscall.Mount(fsname, fsname, "", syscall.MS_BIND, ""))
+	handleErrors(os.MkdirAll(fsname+"/oldrootfs", 0700))
+	handleErrors(syscall.PivotRoot(fsname, fsname+"/oldrootfs"))
+	handleErrors(os.Chdir("/"))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
+	handleErrors(cmd.Run())
+	handleErrors(syscall.Unmount(fsname, 0))
+}
+
+// cgroup ... Create a CGroup for the spawned process
+func cgroup() {
+	cgroups := "/sys/fs/cgroups"
+	pids := filepath.Join(cgroups, "pids")
+
+	handleErrors(os.Mkdir(filepath.Join(pids, cid), 0755))
+	handleErrors(ioutil.WriteFile(filepath.Join(pids, "pids/"+cid+".max"), []byte(proc_max), 0700))
+	handleErrors(ioutil.WriteFile(filepath.Join(pids, "pids/notify_on_release"), []byte("1"), 0700))
+	handleErrors(ioutil.WriteFile(filepath.Join(pids, "pids/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
