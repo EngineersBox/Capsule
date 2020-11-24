0ad2ca8a51f63bc6ceecfe410b6a5ae76c7dff6d
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2020-11-24 23:21:42 +1100

    Added file perm constants

--- src/lib/container.go
+++ src/lib/container.go
@@ -12,6 +12,11 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	mkdirPerm     os.FileMode = 0755
+	writeFilePerm os.FileMode = 0700
+)
+
 // RunState ... Object describing state of a container
 type RunState struct {
 	running  bool
@@ -78,18 +83,18 @@ func (c *Container) CreateCGroup() {
 
 	c.invoker.HandledInvocationGroup(
 		// Create CGroup sub-directory
-		os.Mkdir(filepath.Join(pids, c.name), 0755),
+		os.Mkdir(filepath.Join(pids, c.name), mkdirPerm),
 		// Set maximum child processes
-		ioutil.WriteFile(filepath.Join(pids, c.name+"/pids.max"), []byte(c.props.procMax), 0700),
-		ioutil.WriteFile(filepath.Join(pids, c.name+"/notify_on_release"), []byte("1"), 0700),
-		ioutil.WriteFile(filepath.Join(pids, c.name+"/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700),
+		ioutil.WriteFile(filepath.Join(pids, c.name+"/pids.max"), []byte(c.props.procMax), writeFilePerm),
+		ioutil.WriteFile(filepath.Join(pids, c.name+"/notify_on_release"), []byte("1"), writeFilePerm),
+		ioutil.WriteFile(filepath.Join(pids, c.name+"/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), writeFilePerm),
 		// Create memory sub-directory
-		os.Mkdir(filepath.Join(memory, c.name), 0755),
+		os.Mkdir(filepath.Join(memory, c.name), mkdirPerm),
 		// Set the maximum memory for the container
-		ioutil.WriteFile(filepath.Join(memory, c.name+"memory.limit_in_bytes"), []byte(c.props.memMax), 0700),
+		ioutil.WriteFile(filepath.Join(memory, c.name+"memory.limit_in_bytes"), []byte(c.props.memMax), writeFilePerm),
 		// Create net_cls sub-directory
-		os.Mkdir(filepath.Join(netCls, c.name), 0755),
+		os.Mkdir(filepath.Join(netCls, c.name), mkdirPerm),
 		// Set the network id to identify packets from this container
-		ioutil.WriteFile(filepath.Join(netCls, c.name+"net_cls.classid"), []byte(string(c.cls)), 0700),
+		ioutil.WriteFile(filepath.Join(netCls, c.name+"net_cls.classid"), []byte(string(c.cls)), writeFilePerm),
 	)
 }
