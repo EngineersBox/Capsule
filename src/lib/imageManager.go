70f0a6bb0bb8a6ff6d1633624ca1c9592ba5ce27
Jack Kilrain <jack.kilrain@instaclustr.com>
2020-11-27 16:58:26 +1100

    Split CreateIso() to method to create filesystem and FS identifier

--- src/lib/imageManager.go
+++ src/lib/imageManager.go
@@ -9,13 +9,49 @@ import (
 	"github.com/diskfs/go-diskfs/disk"
 	"github.com/diskfs/go-diskfs/filesystem"
 	"github.com/diskfs/go-diskfs/filesystem/iso9660"
+	"github.com/google/uuid"
 )
+
+// BlockSizes ... Logical block sizes
+type BlockSizes struct {
+	Bs2048 int64
+	Bs4096 int64
+	Bs8192 int64
+}
+
+var blockSizes BlockSizes = BlockSizes{2048, 4096, 8192}
+
+const basePartitionIdentifier string = "BASE_FS_DECL.txt"
 
 // ImageManager ... Create/destroy/manipulate disk images
 type ImageManager struct {
+	ID          uuid.UUID
 	VolumeLabel string
 	FsDisk      *disk.Disk
 	Fs          filesystem.FileSystem
+}
+
+// CreateNewFileSystem ... Create an ISO9660 compatible filespace with a single partition
+func (i *ImageManager) CreateNewFileSystem() {
+	// the following line is required for an ISO, which may have logical block sizes
+	// only of 2048, 4096, 8192
+	i.FsDisk.LogicalBlocksize = blockSizes.Bs2048
+	fspec := disk.FilesystemSpec{
+		Partition:   0,
+		FSType:      filesystem.TypeISO9660,
+		VolumeLabel: i.VolumeLabel,
+	}
+	newFs, err := i.FsDisk.CreateFilesystem(fspec)
+	handler.HandleErrors(err)
+	i.Fs = newFs
+}
+
+// InitialiseFsPartitionIdentifier ... Create a FS identifier from ID + VolumeLabel + FsDisk.Table.Type and write at root dir "/"
+func (i *ImageManager) InitialiseFsPartitionIdentifier() {
+	rw, err := i.Fs.OpenFile(basePartitionIdentifier, os.O_CREATE|os.O_RDWR)
+	content := []byte(string(i.ID.String() + i.VolumeLabel + i.FsDisk.Table.Type()))
+	_, err = rw.Write(content)
+	handler.HandleErrors(err)
 }
 
 // CreateIso ... Create an ISO9660 disk image
@@ -29,22 +65,7 @@ func (i *ImageManager) CreateIso(diskImg string) {
 	handler.HandleErrors(err)
 	i.FsDisk = newDisk
 
-	// the following line is required for an ISO, which may have logical block sizes
-	// only of 2048, 4096, 8192
-	i.FsDisk.LogicalBlocksize = 2048
-	fspec := disk.FilesystemSpec{
-		Partition:   0,
-		FSType:      filesystem.TypeISO9660,
-		VolumeLabel: i.VolumeLabel,
-	}
-	newFs, err := i.FsDisk.CreateFilesystem(fspec)
-	handler.HandleErrors(err)
-	i.Fs = newFs
-
-	rw, err := i.Fs.OpenFile("demo.txt", os.O_CREATE|os.O_RDWR)
-	content := []byte("demo")
-	_, err = rw.Write(content)
-	handler.HandleErrors(err)
+	i.CreateNewFileSystem()
 
 	iso, ok := i.Fs.(*iso9660.FileSystem)
 	if !ok {
