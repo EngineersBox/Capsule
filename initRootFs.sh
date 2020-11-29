df88d6870c3f3fe11fd26152be0765ab5f0c33d4
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2020-11-29 16:35:21 +1100

    Fixed pathing issues to new container dir when mirroring Alpine fs

--- initRootFs.sh
+++ initRootFs.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 CONTAINER_NAME=$1
 
+echo "[Docker Install] Checking installation of Docker..."
 if [ -x "$(command -v docker)" ]; then
-    echo "Updating docker"
-    docker update
+    echo "[Docker Install] Docker installed, skipping installation"
 else
-    echo "Installing docker"
+    echo "[Docker Install] Installing Docker..."
 
     if [[ "$OSTYPE" == "darwin"* ]]; then
         # Install docker with brew if on Mac OS X
         brew install docker
+        echo "[Docker Install] Installed Docker"
     elif [[ "$OSTYPE" == "linux"* || "$OSTYPE" == "bsd"* ]]; then
         # Instacll docker with apt if on Linux or BSD
         apt install docker.io
+        echo "[Docker Install] Installed Docker"
     else
-        echo "Cannot install docker, please install it manually"
+        echo "[Docker Install] Cannot install Docker, please install it manually"
     fi
 fi
 
 # Retrieve the Alpine Linux image distro
 docker pull alpine:latest
 # Get the overlayfs location
-OVERLAY_ID = $(docker image inspect alpine:latest -f {{.GraphDriver.Data.UpperDir}} | sed "s/\'//g")
+OVERLAY_ID=$(docker image inspect alpine:latest -f '{{.GraphDriver.Data.UpperDir}}' | sed "s/\'//g")
 
 # Copy the overlayfs to the container location
 cp -r $OVERLAY_ID /root/$CONTAINER_NAME
+echo "[FS Init] Copied overlay filesystem from Alpine to [/root/$CONTAINER_NAME]"
 
 # Create an identifier in the container
-CONTAINER_IDENTIFIER = "testContainer"
+CONTAINER_IDENTIFIER="testContainer"
 mkdir -p /root/$CONTAINER_NAME/$CONTAINER_IDENTIFIER
+echo "[FS Init] Created identifier in container [/root/$CONTAINER_NAME/$CONTAINER_IDENTIFIER]"
+echo "[FS Init] Filesystem creation for [$CONTAINER_NAME] complete"
